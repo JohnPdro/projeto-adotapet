b1051906988155b8438bb80d20e6ddb977963e38
luiseduardomattos <114542913+luiseduardomattos@users.noreply.github.com>
2023-06-16 20:45:08 -0300

    cabecalho.php
    criação do cabeçalho.php

---
+++ projeto/includes/cabecalho.php
@@ -0,0 +1,55 @@
+<!-- ABRIGOS -->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="pedro.css">
+</head>
+
+<body>
+    <header>
+        <div class="menuInterativo">
+            <a href="" id="animacaoLogo"><img src="../../imagens/img/logo.png" width="200" alt=""></a>
+            <nav>
+                <a href="../../projeto/homePage/homePage.html">HOME</a>
+                <a href="../../projeto/formulario/formulario.html">DOE PET</a>
+                <a href="../../projeto/abrigos/abrigos.html">ABRIGOS</a>
+                <a href="../../projeto/sobre/sobre.html">SOBRE NÓS</a>
+                <a href="../../projeto/login/login.html">ENTRAR</a>
+                <a href="../../projeto/cadastro/cadastro.html">CADASTRAR</a>
+        </div>
+
+        <!-- FORMULARIO -->
+
+        <!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https: //fonts.googleapis.com/css2?family= Lilita +One & display=swap" rel="stylesheet">
+    <title>Formulario</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<header>
+    <a href="../projeto/homePage/homePage.html" id="logo"><img src="../../imagens/img/logo.png" width="200"
+            alt=""></a>
+    <nav>
+        <a href="../../projeto/homePage/homePage.html">HOME</a>
+        <a href="#">DOE PET</a>
+        <a href="../../projeto/abrigos/abrigos.html">ABRIGOS</a>
+        <a href="../../projeto/sobre/sobre.html">SOBRE NÓS</a>
+        <a href="../../projeto/login/login.html">ENTRAR</a>
+        <a href="../../projeto/cadastro/cadastro.html">CADASTRAR</a>
+    </nav>
+</header>    
