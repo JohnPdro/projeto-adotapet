46b45285618c5c7dbe9da20552c18e84f4efb1b1
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-06-16 21:00:57 -0300

    .

--- newForm/form.php
+++ newForm/form.php
@@ -114,13 +114,8 @@
             <input id="btnEnviar" type="submit" value="Registrar Pet">
         </form>
     </main>
-</body>
 
-<footer>
-    <div id="guiaFooter">
-    <img src="../imagens/img/logo.png" width="200" alt="">
-    <p>Bem-vindo ao nosso site de adoção de animais! Somos uma equipe de iniciantes em programação que se uniu para criar esta plataforma com o objetivo de ajudar animais a encontrar um lar amoroso. Nosso site é sem fins lucrativos e foi criado com muito esforço e dedicação para a nossa comunidade. Esperamos que você goste da experiência em nosso site e, se puder, ajude a compartilhar essa ideia para que mais pessoas possam adotar e cuidar desses animais tão especiais. Juntos podemos fazer a diferença!</p>
-    </div>
-</footer>
 
-</html>
+<?php
+include "../projeto-adotapet/projeto/includes/rodape.php";
+?>
