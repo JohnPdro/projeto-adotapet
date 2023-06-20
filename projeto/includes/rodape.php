90a7b143f19ce7bacc212880123481dc486652f0
Gustavo H. Fazam <112525498+GuFazam@users.noreply.github.com>
2023-06-20 16:05:13 -0300

    testando os includes

--- projeto/includes/rodape.php
+++ projeto/includes/rodape.php
@@ -1,8 +1,30 @@
-<footer>
+<!-- <footer>
     <div id="guiaFooter">
     <img src="../imagens/img/logo.png" width="200" alt="">
     <p>Bem-vindo ao nosso site de adoção de animais! Somos uma equipe de iniciantes em programação que se uniu para criar esta plataforma com o objetivo de ajudar animais a encontrar um lar amoroso. Nosso site é sem fins lucrativos e foi criado com muito esforço e dedicação para a nossa comunidade. Esperamos que você goste da experiência em nosso site e, se puder, ajude a compartilhar essa ideia para que mais pessoas possam adotar e cuidar desses animais tão especiais. Juntos podemos fazer a diferença!</p>
     </div>
 </footer>
 </body>
+</html> -->
+
+<link rel="stylesheet" href="rodape.css">
+
+<footer>
+    <ul class="ulFooter">
+        <li class="liFooter"><a href="../../projeto/homePage/homePage.html">HOME</a></li>
+        <li class="liFooter"><a href="">DOE PET</a></li>
+        <li class="liFooter"><a href="../../projeto/abrigos/pedro.html">ABRIGOS</a></li>
+        <li class="liFooter"><a href="../../projeto/sobre/sobre.html">SOBRE NÓS</a></li>
+        <li class="liFooter"><a href="../../projeto/login/login.html">ENTRAR</a></li>
+        <li class="liFooter"><a href="../../projeto/cadastro/cadastro.html">CADASTRAR</a></li>
+    </ul>
+    <p>Somos uma provedora de hospedagem de sites e nossa missão é promover o sucesso a todos que
+        querem começar sua jornada online. Para isso, buscamos sempre melhorar a tecnologia dos nossos servidores,
+        oferecer um suporte profissional e tornar a experiência dos nossos clientes com a hospedagem de sites a
+        melhor
+        de todas.</p>
+    <a href="../../projeto/homePage/homePage.html" ><img src="../../imagens/img/logo.png" width="200" alt=""></a>
+    </div>
+</footer>
+
 </html>
